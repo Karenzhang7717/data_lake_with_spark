4e29989e412abc4587741169abf76d6bd45f34c9
Karenzhang7717 <kzhang@citrine.io>
2021-12-30 10:37:38 -0700

    added doc string

--- etl.py
+++ etl.py
@@ -13,8 +13,11 @@ config.read('dl.cfg')
 os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
-
 def create_spark_session():
+    """
+    sets up a Spark Session.
+    :return: the Spark sessin object
+    """
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -22,6 +25,13 @@ def create_spark_session():
     return spark
 
 def process_song_data(spark, input_data, output_path):
+    """
+    Process song data and generates two dimension tables, songs and artists. Save the parquet files to S3 Bucket.
+    :param spark: The Spark session object.
+    :param input_data: Input data path.
+    :param output_path: Output data path.
+    :return: None
+    """
     # get filepath to song data file
     songSchema = R([
     Fld("num_songs",Int()),
@@ -53,7 +63,16 @@ def process_song_data(spark, input_data, output_path):
 
     # write artists table to parquet files
     artists_table.write.mode("overwrite").parquet(output_path+"artists_table/")
+
 def process_log_data(spark, input_data, output_path):
+    """
+    Process log data to get two dimension tables, users and time, as well as one fact table, songplays.
+    Save the output as parquet files to S3.
+    :param spark: The Spark session object.
+    :param input_data: Input data path.
+    :param output_path: Output data path.
+    :return: None
+    """
     # read log data file
     df_log = spark.read.json("s3a://udacity-dend/log_data/*/*/*.json")
 
@@ -90,36 +109,35 @@ def process_log_data(spark, input_data, output_path):
     # read in artist data
     artist_df = spark.read.parquet(output_path + "artists_table/")
 
-    # add artist name column by joining artist table and song table
-    song_df = song_df.join(artist_df, )
-
     #create songplay_id column for df_log
     df_log = df_log.withColumn('songplay_id', monotonically_increasing_id())
-    df_log.createTempView("df_log")
-    song_df.createTempView("song_df")
+
+    #create Tempview tables for running sql queries in Hive
+    df_log.createTempView("staging_events")
+    song_df.createTempView("songs")
+    artist_df.createTempView("artists")
 
     # extract columns from joined song and log datasets to create songplays table; include year and month to allow parquet partitioning
     songplays_table = spark.sql("""
         SELECT
-            l.songplay_id,
-            l.start_time,
-            l.userId as user_id,
-            l.level,
+            se.songplay_id,
+            se.start_time,
+            se.userId as user_id,
+            se.level,
             s.song_id,
             s.artist_id,
-            l.sessionId as session_id,
-            l.location,
-            l.userAgent as user_agent,
+            se.sessionId as session_id,
+            se.location,
+            se.userAgent as user_agent,
             year(l.start_time) as year,
             month(l.start_time) as month
-        FROM df_log l
-        LEFT JOIN song_df s 
-            INNER JOIN artists ON song_df.artist_id = artists.artist_id AS tmp
-        
+        FROM staging_events se
+        LEFT JOIN (songs s
+            INNER JOIN artists ON song_df.artist_id = artists.artist_id) AS tmp
         ON
-            l.song = tmp.title AND
-            l.artist = tmp.artist_name
-        WHERE l.page='NextSong'
+            se.song = tmp.title AND
+            se.artist = tmp.artist
+        WHERE se.page='NextSong'
     """)
 
     # write songplays table to parquet files partitioned by year and month
