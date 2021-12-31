2390411e6e37f6358c8b86b6a69e02ee85115f7a
Karenzhang7717 <kzhang@citrine.io>
2021-12-31 12:12:03 -0700

    fixed bugs in etl

--- etl.py
+++ etl.py
@@ -28,43 +28,44 @@ def process_song_data(spark, output_path):
     """
     Process song data and generates two dimension tables, songs and artists. Save the parquet files to S3 Bucket.
     :param spark: The Spark session object.
-    :param input_data: Input data path.
     :param output_path: Output data path.
     :return: None
     """
     # get filepath to song data file
     songSchema = R([
-    Fld("num_songs",Int()),
-    Fld("artist_id",Str()),
-    Fld("artist_latitude",Dbl()),
-    Fld("artist_longitude",Dbl()),
-    Fld("artist_location",Str()),
-    Fld("artist_name",Str()),
-    Fld("title",Str()),
-    Fld("duration",Dbl()),
-    Fld("year",Int()),
+        Fld("num_songs", Int()),
+        Fld("artist_id", Str()),
+        Fld("artist_latitude", Dbl()),
+        Fld("artist_longitude", Dbl()),
+        Fld("artist_location", Str()),
+        Fld("artist_name", Str()),
+        Fld("title", Str()),
+        Fld("duration", Dbl()),
+        Fld("year", Int()),
 
     ])
 
     # read song data file, create a staging_songs table first
-    staging_songs = spark.read.json("s3a://udacity-dend/song_data/A/*/*/*.json",schema=songSchema)
+    staging_songs = spark.read.json("s3a://udacity-dend/song_data/A/A/*/*.json", schema=songSchema)
+    staging_songs = staging_songs.withColumn('song_id', monotonically_increasing_id())
+
     staging_songs.registerTempTable("staging_songs")
 
     # dedup songs data and create songs table
     songs_table = spark.sql("""SELECT DISTINCT song_id, title, artist_id, year, duration 
                                 FROM 
                                     (SELECT 
-                                        title, artist_id, year, duration, ROW_NUMBER() OVER(PARTITION BY song_id ORDER BY title desc) AS sequence 
+                                        song_id, title, artist_id, year, duration, ROW_NUMBER() OVER(PARTITION BY song_id ORDER BY title desc) AS sequence 
                                     FROM 
                                         staging_songs) AS ss
                                      WHERE sequence =1   
                             """)
 
     # write songs table to parquet files partitioned by year and artist
-    songs_table.write.mode("overwrite").partitionBy("year", "artist_id").parquet(output_path+"songs_table")
+    songs_table.write.mode("overwrite").partitionBy("year", "artist_id").parquet(output_path + "songs_table")
 
-    #dedup artists data and create artists table
-    artists_table = spark.sql("""SELECT DISTINCT 
+    # dedup artists data and create artists table
+    artists_table = spark.sql("""SELECT DISTINCT artist_id, artist, location, latitude, longitude
                                     FROM 
                                         (SELECT 
                                             artist_id, artist_name as artist, artist_location as location, artist_latitude as latitude, artist_longitude as longitude,
@@ -74,19 +75,19 @@ def process_song_data(spark, output_path):
                             """)
 
     # write artists table to parquet files
-    artists_table.write.mode("overwrite").parquet(output_path+"artists_table/")
+    artists_table.write.mode("overwrite").parquet(output_path + "artists_table/")
+
 
 def process_log_data(spark, output_path):
     """
     Process log data to get two dimension tables, users and time, as well as one fact table, songplays.
     Save the output as parquet files to S3.
     :param spark: The Spark session object.
-    :param input_data: Input data path.
     :param output_path: Output data path.
     :return: None
     """
     # read log data file
-    staging_events= spark.read.json("s3a://udacity-dend/log_data/*/*/*.json")
+    staging_events = spark.read.json("s3a://udacity-dend/log_data/*/*/*.json")
 
     # filter by actions for song plays
     staging_events = staging_events.filter(staging_events.page == "NextSong")
@@ -100,19 +101,19 @@ def process_log_data(spark, output_path):
 
     # create a TempTable for sql queries
     staging_events.registerTempTable("staging_events")
-    
-    #dedup users data and create users table
+
+    # dedup users data and create users table
     users_table = spark.sql("""SELECT DISTINCT user_id, first_name, last_name, gender, level
                                 FROM 
                                     (SELECT 
                                             userId as user_id, firstName as first_name, lastName as last_name, gender, level,
-                                            ROW_NUMBER() OVER (PARTITION BY userId ORDER BY last_name) AS sequence
+                                            ROW_NUMBER() OVER (PARTITION BY userId ORDER BY lastName) AS sequence
                                             FROM staging_events) AS se
-                                            WHERE sequence =1 AND userId IS NOT NULL
+                                            WHERE sequence =1 AND user_id IS NOT NULL
                                 """)
 
     # write users table to parquet files
-    users_table.write.mode("overwrite").parquet(output_path+"users_table/")
+    users_table.write.mode("overwrite").parquet(output_path + "users_table/")
 
     # dedup time data and create time table
     time_table = spark.sql("""SELECT DISTINCT timestamp FROM staging_events WHERE timestamp IS NOT NULL
@@ -120,15 +121,19 @@ def process_log_data(spark, output_path):
 
     # extract columns from timestamp
     time_table = time_table.select(
-                    col('timestamp').alias('start_time'),
-                    hour('timestamp').alias('hour'),
-                    dayofmonth('timestamp').alias('day'),
-                    weekofyear('timestamp').alias('week'),
-                    month('timestamp').alias('month'),
-                    year('timestamp').alias('year'))
+        col('timestamp').alias('start_time'),
+        hour('timestamp').alias('hour'),
+        dayofmonth('timestamp').alias('day'),
+        weekofyear('timestamp').alias('week'),
+        month('timestamp').alias('month'),
+        year('timestamp').alias('year'))
 
     # write time table to parquet files partitioned by year and month
     time_table.write.mode("overwrite").partitionBy("year", "month").parquet(output_path + "time_table/")
+
+    # drop Tempviews for songs and artists in case it existed already
+    spark.catalog.dropTempView("songs")
+    spark.catalog.dropTempView("artists")
 
     # read in song data to use for songplays table and create table
     song_df = spark.read.parquet(output_path + 'songs_table/')
@@ -142,19 +147,20 @@ def process_log_data(spark, output_path):
     songplays_table = spark.sql("""
         SELECT DISTINCT
             se.songplay_id,
-            se.start_time,
+            se.timestamp as start_time,
             se.userId as user_id,
             se.level,
-            s.song_id,
-            s.artist_id,
+            tmp.song_id,
+            tmp.artist_id,
             se.sessionId as session_id,
             se.location,
             se.userAgent as user_agent,
-            year(se.start_time) as year,
-            month(se.start_time) as month
+            year(se.timestamp) as year,
+            month(se.timestamp) as month
         FROM staging_events se
-        LEFT JOIN (songs s
-            INNER JOIN artists ON song_df.artist_id = artists.artist_id) AS tmp
+        LEFT JOIN (SELECT 
+            s.song_id, a.artist_id, s.title, s.duration, a.artist FROM songs s
+            INNER JOIN artists a ON s.artist_id = a.artist_id) AS tmp
         ON
             se.song = tmp.title AND
             se.artist = tmp.artist
@@ -162,7 +168,7 @@ def process_log_data(spark, output_path):
     """)
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table.write.partitionBy('year','month').mode("overwrite").parquet(output_path + 'songplays_table/')
+    songplays_table.write.partitionBy('year', 'month').mode("overwrite").parquet(output_path + 'songplays_table/')
 
 
 def main():
